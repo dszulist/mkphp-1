e0a3e4917a3d5e38ad47b26d34599dd273b81914
jkonefal <j.konefal@madkom.pl>
2011-10-25 06:00:14 +0000

    Przeniesienie pozostałych funkcji do MK_Db_PDO

--- Db/PDO.php
+++ Db/PDO.php
@@ -25,9 +25,12 @@ class MK_Db_PDO {
 	 */
 	private $_transOk = false;
 
+	private $_sqlIgnoreClass = array('MK_Db_PDO');
+
 	protected $db = null;
 
 	public function __construct(){
+		$this->_prepareSortParam();
 		// Uruchomienie licznika uruchamiania zapytania SQL
 		$timeStart = microtime(true);
 
@@ -38,6 +41,15 @@ class MK_Db_PDO {
 		$execTime = microtime(true) - $timeStart;
 		// Zwrócenie szczegółowego komunikatu w konsoli FireBug-a
 		$this->fireBugSqlDump("MK_Db_PDO_Singleton::getInstance()", '', array(), $execTime);
+	}
+
+
+	/**
+	 * Ustawienie większej ilości klas do ignorowania dla fireBugSqlDump()
+	 * @param array $classArray
+	 */
+	public function setMoreSqlIgnoreClass($classArray) {
+		$this->_sqlIgnoreClass = array_merge($this->_sqlIgnoreClass, $classArray);
 	}
 
 
@@ -369,7 +381,7 @@ class MK_Db_PDO {
 	 *   true  - monitoruje błędy SQL,
 	 *   false - wymuszenie odrzucenia wszystkich SQL-i w transakcji
 	 *
-	 * @return 
+	 * @return
 	 */
 	public function transComplete( $commit = true ) {
 		$this->fireBugSqlDump("transComplete");
@@ -779,6 +791,315 @@ class MK_Db_PDO {
 		}
 		$splittedSql .= ')';
 		return $splittedSql;
+	}
+
+
+	/**
+	 * Wywoływana gdy nie znajdzie metody w klasach dziedziczących.
+	 * Wykorzystujemy ją do budowania dynamicznych zapytań do bazy.
+	 * Przy kładowo jeżeli wywołamy metodę findRowByIs w modelu ResolutionDb to budowane jest zapytanie "SELECT * FROM swpirb_resolution WHERE id = ?".
+	 * Trzeba pamiętać aby klasa, z której wywoływana jest metoda musi zawierać atrybut "_tableName" ustawiony na protected
+	 *
+	 * Dynamiczne metody:
+	 * 	- findRowBy - zwraca jeden wiersz np findRowByIdAndState
+	 * 	- findRowsBy - zwraca wiersze spełniające warunki np findRowsByIdAndState
+	 *
+	 * Kolejne parametry rozdzialamy słowem And
+	 *
+	 * @param string $name - nazwa wywoływanej metody
+	 * @param array $arguments - parametry przekazywane do wywoływanej metody
+	 * @return array
+	 */
+	public function __call($name, $arguments) {
+		if (!preg_match('/^(find[a-zA-Z]+)By/', $name, $match)) {
+			throw new Exception('Nie odnaleziono funkcji ' . $name);
+		}
+		$methodName = '_' . $match[1];
+		if (method_exists($this, $methodName)) {
+			return $this->{$methodName}($name, $arguments);
+		}
+
+		throw new Exception('Nie odnaleziono funkcji ' . $name);
+	}
+
+
+	/**
+	 * Wywoływana gdy wywołamy metodę zaczynającą się od "findRowsBy".
+	 * Zwraca rekordy spełniające podane warunki.
+	 * Nazwy parametrów znajdują się nazwie f-cji rozdzielone słowem AND.
+	 *
+	 * Przykładowa nazwa funkcji wywołana z klasy ResolutionDb "findRowsByIdAndState($id, $state)",
+	 * zwróci wyniki na podstawie zapytania: SELECT * FROM swpirb_resolution WHERE id = ? AND state = ?
+	 *
+	 * @param string $name
+	 * @param array $arguments
+	 * @return array
+	 */
+	private function _findRows($name, array $arguments) {
+		$sql = $this->_prepareSql(
+			str_replace('findRowsBy', '', $name), $arguments
+		);
+		return $this->GetRows($sql, $arguments);
+	}
+
+
+	/**
+	 * Wywoływana gdy wywołamy metodę zaczynającą się od "findRowBy".
+	 * Zwraca jeden rekord spełniający podane warunki.
+	 * Nazwy parametrów znajdują się nazwie f-cji rozdzielone słowem AND.
+	 *
+	 * Przykładowa nazwa funkcji wywołana z klasy ResolutionDb "findRowByIdAndState($id, $state)",
+	 * zwróci wyniki na podstawie zapytania: SELECT * FROM swpirb_resolution WHERE id = ? AND state = ? LIMIT 1
+	 *
+	 * @param string $name
+	 * @param array $arguments
+	 * @return array
+	 */
+	private function _findRow($name, array $arguments) {
+		$sql = $this->_prepareSql(
+			str_replace('findRowBy', '', $name), $arguments
+		) . ' LIMIT 1';
+		return $this->GetRow($sql, $arguments);
+	}
+
+
+	/**
+	 * Zwraca tablicę zawierająca tylko te elementy, które pokrywają się z kolumnami tabeli
+	 *
+	 * @param array $args
+	 * @return array
+	 */
+	protected function _getDataForColumns(array $args) {
+		$data = array_intersect_key($args, array_flip($this->_tableColumns));
+
+		//puste string'i zamieniamy na null'e
+		foreach ($data as &$colValue) {
+			if (is_null($colValue) || (empty($colValue) && strlen($colValue) === 0)) {
+				$colValue = null;
+			}
+		}
+		unset($colValue);
+
+		return $data;
+	}
+
+
+	/**
+	 *
+	 * Zebranie nazw pól po których ma się odbyć wyszukiwanie
+	 *
+     * @param Array     $ia_queryParams
+     *
+	 * @return Array
+	 */
+	protected function _prepareFieldsFromQueryParams(array $ia_queryParams) {
+		$oa_fields = array();
+
+		if (!empty($ia_queryParams['fields']) && !empty($ia_queryParams['query']) && mb_strlen($ia_queryParams['query']) > 0) {
+			$a_fieldsTmp = json_decode($ia_queryParams['fields']);
+
+			if (is_array($a_fieldsTmp) && count($a_fieldsTmp) > 0) {
+				foreach ($a_fieldsTmp as $s_field) {
+					if (in_array($s_field, $this->fieldsCanBeSearched)){
+						$oa_fields[] = $s_field;
+					}
+				}
+			}
+		}
+
+		return $oa_fields;
+	}
+
+
+	/**
+	 * Jeżeli w rejestrze nie ma ustawionej kolumny sortowania,
+	 * to ustawiamy ją na podstawie stałej SORT_COLUMN pochodzącej z klasy dziecka
+	 */
+	private function _prepareSortParam() {
+		if (!MK_Registry::isRegistered('sort') || MK_Registry::get('sort') === null) {
+			$const = get_class($this) . '::SORT_COLUMN';
+			if (defined($const)) {
+				MK_Registry::set('sort', constant($const));
+			}
+		}
+	}
+
+
+	/**
+	 * Zamienia wielką literę na małą dodając przed nią _.
+	 * Np: A -> _a
+	 *
+	 * @param array $match
+	 * @return string
+	 */
+	private function _replaceUppercase($match) {
+		return '_' . strtolower($match[1]);
+	}
+
+
+	/**
+	 * Przygotowuje SQL'a na podstawie podstawie podanych nazw parametrów w f-cji.
+	 *
+	 * @param string $name
+	 * @param array $arguments
+	 * @return string
+	 */
+	private function _prepareSql($name, array $arguments) {
+		$paramsNames = explode('And', $name);
+		$sql = 'SELECT * FROM ' . $this->_tableName . ' WHERE ';
+		$sqlAnd = '';
+
+		$i = 0;
+		foreach ($paramsNames as $paramName) {
+			$paramName{0} = strtolower($paramName{0});
+			$paramName = preg_replace_callback('/([A-Z]{1})/', array($this, '_replaceUppercase'), $paramName);
+			$sql .= $sqlAnd . strtolower($paramName) . ' = ? ';
+			$sqlAnd = ' AND ';
+
+			if (!array_key_exists($i, $arguments)) {
+				throw new InvalidArgumentException('Podano nieprawidłową liczbę parametrów');
+			}
+			$i++;
+		}
+
+		return $sql;
+	}
+
+
+	/**
+	 * Grupowanie identyfikatorów:
+	 *  - insert - nie występuje w tablicy $oldIds, ale występuje w $newIds
+	 *  - update - występuje w tablicy $oldIds oraz $newIds
+	 *  - delete - występuje w tablicy $oldIds, ale nie występuje w $newIds
+	 *
+	 * @param Array $newIds Identyfikatory do sprawdzenia
+	 * @param Array $oldIds Identyfikatory pobrane z bazy danych
+	 * @return Array
+	 */
+	public function groupIdsBySql(array $newIds, array $oldIds) {
+		$ret = array(
+			'insert' => array(),
+			'update' => array(),
+			'delete' => array()
+		);
+
+		foreach ($newIds as $nKey => $nValue) {
+			$notInArray = true;
+			foreach ($oldIds as $oKey => $oValue) {
+				if ($nValue == $oValue) {
+					$ret['update'][] = $nValue;
+					unset($oldIds[$oKey]);
+					$notInArray = false;
+					break;
+				}
+			}
+			if ($notInArray) {
+				$ret['insert'][] = $nValue;
+			}
+		}
+		$ret['delete'] = $oldIds;
+
+		return $ret;
+	}
+
+
+	/**
+	 * Zrzucenie SQL-a i jego parametrów do okna firebuga
+	 *
+	 * @param String $dumpName - nazwa wyświetlanej operacji
+	 * @param String $sql - zapytanie SQL
+	 * @param Array $params - parametry zapytania (dane)
+	 */
+	public function fireBugSqlDump($dumpName, $sql="", array $params=array(), $execTime=0) {
+		if (DEVELOPER === true || ( isset($_SESSION['APP_DEBUG']) && $_SESSION['APP_DEBUG'] === true )) {
+			// Odczytanie klasy i metody, w której wyświetlony zostanie komunikat
+			$className = get_class($this);
+			$methodName = '';
+			$filePath = '';
+			$lineNumber = '';
+			$traceList = debug_backtrace();
+			$traceArr = array();
+			foreach ($traceList as $trace) {
+				if (!isset($trace['class']) || in_array($trace['class'], $this->_sqlIgnoreClass)) {
+					continue;
+				}
+				if (count($traceArr) == 0) {
+					$className = $trace['class'];
+					$methodName = isset($trace['function']) ? $trace['function'] : '';
+					$filePath = isset($trace['file']) ? str_replace(SITE_PATH, '', $trace['file']) : '';
+					$lineNumber = isset($trace['line']) ? $trace['line'] : -1;
+				}
+				if (isset($trace['object'])) {
+					unset($trace['object']);
+				}
+				$traceArr[] = $trace;
+			}
+			// Czas generowania SQL-a
+			$sqlTime = microtime(true);
+			$sqlTimeDiff = round($sqlTime - ( isset($_SESSION['sql_last_time']) ? $_SESSION['sql_last_time'] : 0 ), 4);
+			$execTime = round($execTime, 4);
+			// Wyświetlenie komunikatu debug-a
+			if (empty($sql)) {
+				// Debugowanie dodatkowych informacji (utworzenie połączenia, otwarcie/zamknięcie transakcji)
+				if (DB_DEBUG) {
+					FB::info((object) array(
+								'OPERATION' => $dumpName,
+								'BACKTRACE' => $traceArr
+							), "INFO ({$sqlTime} [+{$sqlTimeDiff}]) :: {$filePath}:{$lineNumber} :: {$className}->{$methodName}");
+				}
+			} else {
+				FB::warn((object) array(
+							'OPERATION' => $dumpName,
+							'SQL+PARAMS' => $this->_sqlFormat($this->_prepareFullQuery($sql, $params)),
+							'SQL' => "\n" . $sql . "\n",
+							'PARAMS' => $params,
+							'BACKTRACE' => $traceArr
+						), "SQL ({$sqlTime} [+{$sqlTimeDiff}ms] {{$execTime}ms}) :: {$filePath}:{$lineNumber} :: {$className}->{$methodName}");
+			}
+		}
+	}
+
+
+	/**
+	 * Przełamuje ciagi sql
+	 *
+	 * @param String $query
+	 * @return String
+	 */
+	private function _sqlFormat($query) {
+		$keywords = array("select ", " from ", " left join ", " right join ", " inner join ", " where ", " order by ", " group by ", "insert into ", "update ");
+		foreach ($keywords as $keyword) {
+			if (preg_match("#($keyword*)#i", $query, $matches)) {
+				$query = str_replace($matches[1], "\n" . strtoupper($matches[1]) . "  \t", $query);
+			}
+		}
+		return $query . "\n";
+	}
+
+
+	/**
+	 * Umieszczenie danych z tablicy $params w zapytaniu SQL.
+	 * Pozwala podglądnąć całe zapytanie SQL i skopiować do dalszej analizy.
+	 *
+	 * @param String $sql - zapytanie SQL
+	 * @param Array $params - parametry zapytania
+	 * @return String
+	 */
+	private function _prepareFullQuery($sql, array $params) {
+		if (count($params) == 0) {
+			return $sql;
+		}
+
+		foreach ($params as $param) {
+			if (is_null($param)) {
+				$param = 'NULL';
+			} elseif (!is_numeric($param)) {
+				$param = "'" . str_replace("'", "''", $param) . "'";
+			}
+			$sql = preg_replace('#\?#', $param, $sql, 1);
+		}
+
+		return $sql;
 	}
 
 }
