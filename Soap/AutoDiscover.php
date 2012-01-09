b165bee5128e78dad9127254971a065589552874
bskrzypkowiak <b.skrzypkowiak@madkom.pl>
2012-01-09 13:39:53 +0000

    modyfikacje  i rozbudowa - dodanie serwera

--- Soap/AutoDiscover.php
+++ Soap/AutoDiscover.php
@@ -12,32 +12,63 @@
 Class MK_Soap_AutoDiscover extends  Zend_Soap_AutoDiscover {
 
     /**
+     * Nazwa instancji webservice
+     * @var String
+     */
+    private $_serviceInstance;
+
+    /**
      * Konstruktor sprawdza czy są podane wymagane dane w _GET i uruchamia mechanizm generowania wsdl'a
      *
-     * @param bool $strategy
-     * @param null $uri
-     * @param null $wsdlClass
+     * @param String $strategy
+     * @param Null $uri
+     * @param Null $wsdlClass
      */
-    public function __construct($strategy=null, $uri=null, $wsdlClass=null){
+    public function __construct($strategy='Zend_Soap_Wsdl_Strategy_ArrayOfTypeSequence', $uri=null, $wsdlClass=null){
 
-        if(!isset($_GET['wsdl'])){
-            throw new MK_Exception("MK Webservice");
-        }
         if(!isset($_GET['instance'])){
             throw new MK_Exception("Brak podanej instancjii serwera");
         }
 
-        if($strategy === null){
-            $strategy = 'Zend_Soap_Wsdl_Strategy_ArrayOfTypeSequence';
+        $this->setInstanceName();
+
+        if(!isset($_GET['wsdl'])){
+            $this->createServer();
         }
 
-        try {
-            parent::__construct($strategy, $uri, $wsdlClass);
-            $this->setClass($_GET['instance']);
-            $this->handle();
-        } catch (Exception $ex) {
-            echo $ex->getMessage();
-        }
+        parent::__construct($strategy, $uri, $wsdlClass);
+        $this->setClass($this->_serviceInstance);
+        $this->handle();
     }
 
+
+    /**
+     * Zwraca uri zawierajacego instancje oraz proźbę o wsdl'a
+     *
+     * @return String
+     */
+    public function getUri(){
+        return parent::getUri() . "?wsdl&instance=" . $this->_serviceInstance;
+    }
+
+    /**
+     * Ustawiamy rodzaj webservice
+     *
+     * @param String|null $instance
+     */
+    public function setInstanceName($instance=null) {
+        if($instance===null && !empty($_GET['instance'])){
+            $instance = $_GET['instance'];
+        }
+        $this->_serviceInstance = $instance;
+    }
+
+    /**
+     * Tworzy instancje i stara się obsłużyć rządanie
+     */
+    private function createServer(){
+        $soap = new Zend_Soap_Server($this->getUri());
+        $soap->setClass($this->_serviceInstance);
+        $soap->handle();
+    }
 }
