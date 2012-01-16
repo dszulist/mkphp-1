915cc5ac4469ce638e94e1e1278651e9ad469a53
jkonefal <j.konefal@madkom.pl>
2012-01-16 13:43:27 +0000

    Zamiana MK_Filter_Html::getInstance()->purify() na MK_Filter_Html::purifyText()

--- Filter/Html.php
+++ Filter/Html.php
@@ -6,6 +6,10 @@
  * Filtrowanie - wykorzystuje HTML Purifier
  *
  * echo MK_Filter_Html::getInstance()->purify('html');
+ * //lub
+ * echo MK_Filter_Html::purifyHtml('html');
+ * //lub bez zakodowanych encji
+ * echo MK_Filter_Html::purifyText('html');
  *
  * @category	MK_Filter
  * @package		MK_Filter_Html
@@ -26,7 +30,7 @@ class MK_Filter_Html {
 			$config = HTMLPurifier_Config::createDefault();
 			$config->set('Core.Encoding', 'UTF-8');
 			$config->set('HTML.Doctype', 'HTML 4.01 Transitional');
-			
+
 			validate_directory(MK_DIR_TEMP . DIRECTORY_SEPARATOR . 'HTMLPurifier');
 			$config->set('Cache.SerializerPath', MK_DIR_TEMP . DIRECTORY_SEPARATOR . 'HTMLPurifier');
 
@@ -34,6 +38,26 @@ class MK_Filter_Html {
 		}
 
 		return self::$_singleton;
+	}
+
+	/**
+	 * Filtrowanie treści i pozostanie w postaci Źródła HTML (zamiast "&amp;" będzie "&")
+	 *
+	 * @param string $html
+	 * @return string
+	 */
+	public static function purifyText($html) {
+		return html_entity_decode(self::getInstance()->purify($html), ENT_COMPAT, 'UTF-8');
+	}
+
+	/**
+	 * Filtrowanie treści i pozostanie w postaci zaenkodowanego HTML-a (np. "&amp;")
+	 *
+	 * @param string $html
+	 * @return string
+	 */
+	public static function purifyHtml($html) {
+		return self::getInstance()->purify($html);
 	}
 
 }
