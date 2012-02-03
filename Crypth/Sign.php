6415d58aca0563ad319ca39e3aa85d1e1f35aef0
bskrzypkowiak <b.skrzypkowiak@madkom.pl>
2012-02-03 09:56:29 +0000

    poprawienie komendy

--- Crypth/Sign.php
+++ Crypth/Sign.php
@@ -179,7 +179,8 @@ class MK_Crypth_Sign {
 	            throw new Exception('Nie udało się zapisać pliku tymczasowego do podpisu: ' . $tempFileName);
             }
 
-			exec($this->pathToJava . ' -jar "' . $this->pathToJarSign . '" -in "' .$tempFileName. '" -sign -dsig -p12 "' .$this->pfxFile. '" -p12pass "' .$this->password. '" -keyalias '.$this->keyAlias.' 2>&1', $output, $returnCode);
+            $command = "{$this->pathToJava} -jar '{$this->pathToJarSign}' -in '{$tempFileName}' -sign -dsig -p12 '{$this->pfxFile}' -p12pass '{$this->password}' -keyalias {$this->keyAlias} 2>&1";
+			exec($command, $output, $returnCode);
 			
 			if ($returnCode != '0'){
 				throw new Exception('Niepowiodło się podpisanie dokumentu, output:'.PHP_EOL.$output);
