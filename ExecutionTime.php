e0baa5114f684e1fa4a7aa20d6126bc33425eeb3
jkonefal <j.konefal@madkom.pl>
2012-06-04 13:44:51 +0000

    Dodanie ExecutionTime

---
+++ ExecutionTime.php
@@ -0,0 +1,43 @@
+<?php
+
+/**
+ * Klasa pomocna przy mierzeniu czasu wykonania kodu
+ */
+class MK_ExecutionTime
+{
+	/**
+	 * Startowy czas w sekundach
+	 *
+	 * @var float
+	 */
+	private $_startTime;
+
+
+	/**
+	 * W konstruktorze ustawiany jest czas startowy
+	 */
+	public function __construct() {
+		$this->_startTime = $this->_getActualTime();
+	}
+
+
+	/**
+	 * Zwraca różnicę czasu od stworzenia obiektu do wywołania tej metody
+	 *
+	 * @return float
+	 */
+	public function getDifferenceTime() {
+		return round($this->_getActualTime() - $this->_startTime, 3);
+	}
+
+
+	/**
+	 * Zwraca aktualny czas w sekundach
+	 *
+	 * @return float
+	 */
+	private function _getActualTime() {
+		list($usec,$sec) = explode(" ",microtime());
+		return ((float)$usec + (float)$sec);
+	}
+}
