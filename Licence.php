54659b4d933108098137cb7cdb354fb1a3b560e4
jkonefal <j.konefal@madkom.pl>
2012-06-13 14:01:53 +0000

    Wyłączenie sprawdzania licencji na prośbę Łukasza

--- Licence.php
+++ Licence.php
@@ -5,10 +5,10 @@
  *
  * Klasa do zarzadzania i obsługi licencjami aplikacji
  *
- * @category	MK
- * @package		MK_Licence
- * @throws		MK_Exception
- * @author		bskrzypkowiak
+ * @category    MK
+ * @package        MK_Licence
+ * @throws        MK_Exception
+ * @author        bskrzypkowiak
  */
 class MK_Licence {
 
@@ -29,11 +29,9 @@ class MK_Licence {
 		if(isset($this->expireDate)) {
 			return $this->expireDate;
 		}
-
 		if(!preg_match('#^([0-9]{4})([0-9]{2})([0-9]{2})#', $licence, $matches)) {
 			throw new MK_Exception('Nieprawidłowa licencja. Proszę o kontakt z administratorem.');
 		}
-
 		$this->expireDate = $matches[1] . '-' . $matches[2] . '-' . $matches[3];
 		return $this->expireDate;
 	}
@@ -51,12 +49,8 @@ class MK_Licence {
 		if(MK_DEVELOPER === true) {
 			return true;
 		}
-
 		$expireDate = $this->expireDate($licence);
-
-		if(!$this->isValidSignature($licence)
-			|| ($statusInconsistencyLicenseKey == 'stop_application' && strtotime($expireDate) < strtotime(date('Y-m-d')))
-		) {
+		if(!$this->isValidSignature($licence) || ($statusInconsistencyLicenseKey == 'stop_application' && strtotime($expireDate) < strtotime(date('Y-m-d')))) {
 			throw new MK_Exception('Błąd krytyczny. Niezgodna sygnatura licencji! <br/> Skontaktuj się z administratorem.');
 		}
 		return true;
@@ -69,18 +63,18 @@ class MK_Licence {
 	 * @param String $licence
 	 *
 	 * @throws MK_Exception
+	 * @return bool
 	 */
 	public function canUpgrade($taskListPathFile, $licence) {
 		if(!empty($taskListPathFile)) {
-
-			if($this->isSupportActive($licence)) {
+			if(!$this->isSupportActive($licence)) {
 				throw new MK_Exception('Wygasło wsparcie techniczne. Proszę o kontakt z administratorem');
 			}
-
 			if(!$this->isValidSignature($licence)) {
 				throw new MK_Exception('Błąd krytyczny. Niezgodna sygnatura licencji!');
 			}
 		}
+		return true;
 	}
 
 	/**
@@ -91,7 +85,10 @@ class MK_Licence {
 	 * @return Boolean
 	 */
 	public function isSupportActive($licence) {
-		return strtotime($this->expireDate($licence)) < strtotime(date('Y-m-d'));
+		return true;
+		/** Wyłączenie sprawdzania licencji na prośbę Łukasza - 13.06.2012 r.
+		return strtotime($this->expireDate($licence)) >= strtotime(date('Y-m-d'));
+		 */
 	}
 
 	/**
@@ -102,9 +99,12 @@ class MK_Licence {
 	 * @return Boolean
 	 */
 	public function isValidSignature($licence) {
+		return true;
+		/** Wyłączenie sprawdzania licencji na prośbę Łukasza - 13.06.2012 r.
 		$expireDate = $this->expireDate($licence);
 		$validLicence = str_replace('-', '', $expireDate) . md5($expireDate . ' ' . exec('hostname') . ' ' . APP_PATH);
 		return (strlen($licence) == 40 && strcmp($validLicence, $licence) == 0);
+		 */
 	}
 
 }
