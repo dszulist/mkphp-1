20de0bb53c2181acb72bf4f88cf08394771a4d77
jkonefal <j.konefal@madkom.pl>
2012-06-15 10:23:11 +0000

    Klasa sendClientSystemInformations do wysyłania informacji statycznych do Brokera

---
+++ Broker/ProxyClass/KeyVal.php
@@ -0,0 +1,28 @@
+<?php
+/**
+ * Pomocnicza klasa dla sendClientSystemInformations
+ */
+class KeyVal {
+
+	/**
+	 * Klucz, np. 'count_tables_row'
+	 * @var string
+	 */
+	public $key;
+
+	/**
+	 * Wartość, np. '123' lub json_encode(array('table1' => 123, 'table2' => 234))
+	 * Podanie pojedynczej wartości będzie uznane jako:
+	 *  count_tables_row => 123
+	 * Podanie tablicy asocjacyjnej będzie dodatkowo interpretowane jako tablica klucz/wartość w szczegółach GO
+	 * @var string|array
+	 */
+	public $value;
+
+	/**
+	 * Nazwa opcji (niewykorzystywane) - takie samo jak klucz, np. 'count_tables_row'
+	 * @var
+	 */
+	public $options;
+
+}
---
+++ Broker/ProxyClass/sendClientSystemInformations.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Wysłanie informacji z systemu do Brokera
+ */
+class sendClientSystemInformations {
+
+	/**
+	 * Nazwa aplikacji
+	 * @var string
+	 */
+	public $appName;
+
+	/**
+	 * Obiekt KeyVal
+	 * @var array
+	 */
+	public $keyValArray;
+
+	/**
+	 * Przygotowanie sendClientSystemInformations - należy jeszcze uruchomić metodę add()
+	 * @param $appName
+	 */
+	public function __construct($appName = null) {
+		$this->appName = is_null($appName) && defined('APP_NAME') ? APP_NAME : $appName ;
+		$this->keyValArray = array();
+	}
+
+	/**
+	 * Dodanie elementów do wysłania do Brokera
+	 *
+	 * @param      $key - klucz
+	 * @param      $value - pojedyncza wartość lub tablica asocjacyjna
+	 * @param null $options - wartość nieużywana (domyślnie taka sama wartość jak klucz)
+	 * @return \sendClientSystemInformations
+	 */
+	public function add($key, $value, $options = null) {
+		$keyVal = new KeyVal();
+		$keyVal->key = $key;
+		$keyVal->value = is_array($value) ? json_encode($value) : $value;
+		$keyVal->options = empty($options) ? $key : $options;
+		$this->keyValArray[] = $keyVal;
+		return $this;
+	}
+
+}
