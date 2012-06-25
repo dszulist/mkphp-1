a9b7b16279aa8950e143b45bd943c08f14d95792
jkonefal <j.konefal@madkom.pl>
2012-06-25 09:11:22 +0000

    Rozbudowanie zapytań SQL o rejestr zmian w powiadomieniach

--- System/Changelog.php
+++ System/Changelog.php
@@ -5,10 +5,10 @@
  *
  * Model dla tabeli system_changelog
  *
- * @category	MK_System
- * @package		MK_System_Changelog
+ * @category    MK_System
+ * @package        MK_System_Changelog
  *
- * @throws		MK_Db_Exception
+ * @throws        MK_Db_Exception
  */
 class MK_System_Changelog extends MK_Db_PDO {
 
@@ -23,9 +23,23 @@ class MK_System_Changelog extends MK_Db_PDO {
 	 * @return array
 	 */
 	public function getList() {
-		$sql = 'SELECT * FROM ' . $this->tableName
-			. ' ORDER BY id DESC';
+		$sql = 'SELECT c.* FROM ' . $this->tableName . ' c'
+			. ' ORDER BY c.id DESC';
 		return $this->GetRows($sql);
+	}
+
+
+	/**
+	 * Odczytanie listy rejestru zmian powiązanych z identyfikatorami powiadomień
+	 *
+	 * @param array $notificationIds
+	 *
+	 * @return array
+	 */
+	public function getListByNotificationIds(array $notificationIds) {
+		$sql = 'SELECT c.* FROM ' . $this->tableName . ' c'
+			. ' WHERE c.notification_id IN (' . $this->arrayToQueryIn($notificationIds) . ') ORDER BY c.id ASC';
+		return $this->GetRows($sql, $notificationIds);
 	}
 
 }
--- System/Notification.php
+++ System/Notification.php
@@ -20,12 +20,13 @@ class MK_System_Notification extends MK_Db_PDO {
 	/**
 	 * Odczytywanie wszystkich rekordów z tabeli
 	 *
-	 * @param $userId
+	 * @param      $userId
+	 * @param bool $withChangelog - czy pobierać dodatkowo listę rejestru zmian
 	 *
 	 * @throws MK_Exception
 	 * @return array
 	 */
-	public function getList($userId) {
+	public function getList($userId, $withChangelog = true) {
         $userId = (int) $userId;
         if($userId <= 0) {
             throw new MK_Exception('Nieprawidłowe parametry do odczytania listy nowych powiadomień');
@@ -33,7 +34,23 @@ class MK_System_Notification extends MK_Db_PDO {
 
 		$sql = 'SELECT sn.* FROM ' . $this->tableName .' sn '
 			. ' LEFT JOIN system_notification_user snu ON (sn.id = snu.notification_id AND snu.user_id = ?) WHERE snu.user_id IS NULL ORDER BY id DESC';
-		return $this->GetRows($sql, array($userId));
+		$notifications = $this->GetRows($sql, array($userId), 'id');
+
+		// Odczytywanie rejestru zmian dla powiadomień
+		if($withChangelog == true) {
+			$notificationIds = array();
+			foreach($notifications as &$notification) {
+				$notificationIds[] = $notification['id'];
+				$notification['changelogs'] = array();
+			}
+			$changelogDb = new MK_System_Changelog();
+			$changelogs = $changelogDb->getListByNotificationIds($notificationIds);
+			foreach($changelogs as &$changelog) {
+				$notifications[$changelog['notification_id']]['changelogs'][] = $changelog;
+			}
+		}
+
+		return array_values($notifications);
 	}
 
 }
